9bcb81ecd8f794ca3a7882bdc0bc703a2a527394
Jun <xjunsantos@gmail.com>
2023-09-01 12:57:16 +0800

    add delete policies

---
+++ app/Policies/ScheduledClassPolicy.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use App\Models\ScheduledClass;
+
+class ScheduledClassPolicy
+{
+    public function delete(User $user, ScheduledClass $scheduledclass) {
+        return $user->id === $scheduledclass->instructor_id;
+    }
+}
